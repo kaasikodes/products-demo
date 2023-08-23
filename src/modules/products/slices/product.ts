57353b6eeedf9b84b54d4d8069fd26f6e6ea4693
kaasikodes <thisbroisfresh@gmail.com>
2023-08-23 15:15:41 +0100

    fix: fixes improper import in products

--- src/modules/products/slices/product.ts
+++ src/modules/products/slices/product.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { TProduct } from "../../types";
+import { TProduct } from "../types";
 import { PRODUCT_STATUSES, PRODUCT_TYPES } from "../constants";
 import moment from "moment";
 
